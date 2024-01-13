97ff37cc16fba2fad85eb638b29b386b34513a18
kkamara <kamaracomputers@gmail.com>
2024-01-13 12:49:20 +0000

    Render different bottom tabs based on authentication status

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { createBottomTabNavigator, } from '@react-navigation/bottom-tabs'
 
 import home from "./screens/home"
 import login from "./screens/login"
+import logout from "./screens/logout"
 import AuthContext, { Context, } from "./context"
 
 const Stack = createNativeStackNavigator()
@@ -13,7 +14,26 @@ const Tab = createBottomTabNavigator()
 const BottomTabs = () => {
     const { authenticated, } = useContext(Context)
 
-    console.log(authenticated)
+    if (authenticated) {
+        return (
+            <Tab.Navigator
+                initialRouteName='Home'
+                screenOptions={{
+                    tabBarIconStyle: { display: "none" },
+                }}
+            >
+                <Tab.Screen
+                    name="Home"
+                    component={home}
+                />
+                <Tab.Screen
+                    name="Logout"
+                    component={logout}
+                />
+            </Tab.Navigator>
+        )
+    }
+
     return (
         <Tab.Navigator
             initialRouteName='Home'
