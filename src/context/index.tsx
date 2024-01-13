568552ac16cbc4bb1a3c6ee524c47bae290a78ea
kkamara <kamaracomputers@gmail.com>
2024-01-13 12:36:28 +0000

    Update import

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, } from "react"
-import type { PropsWithChildren } from "react"
+import type { PropsWithChildren, } from "react"
 
 export const Context = createContext(null)
 
