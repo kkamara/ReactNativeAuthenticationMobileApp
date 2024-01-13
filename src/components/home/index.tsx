65b4bf3e1abf06839ac7cedf83c08bba992f9e02
kkamara <kamaracomputers@gmail.com>
2024-01-13 12:00:50 +0000

    Update home component name

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -7,7 +7,7 @@ import {
     Text,
 } from "react-native"
 
-export default function Home() {
+export default function HomeComponent() {
     const openUrl = async () => {
         await Linking.openURL("https://www.kelvinkamara.com")
     }
