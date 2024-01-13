a74d7970d68cb0d1263cd53d4bdd21d37490768b
kkamara <kamaracomputers@gmail.com>
2024-01-13 15:41:04 +0000

    Prepare app for local storage setting & getting values

--- src/context/index.js
+++ src/context/index.js
@@ -1,9 +1,18 @@
-import React, { createContext, useState, } from "react"
+import React, {
+    createContext,
+    useState,
+    useEffect,
+} from "react"
+import { AUTH_KEY, } from "../constants"
 
 export const Context = createContext(null)
 
 const AuthContext = ({ children, }) => {
     const [authenticated, setAuthenticated] = useState(false)
+
+    useEffect(() => {
+
+    }, [])
 
     return <Context.Provider value={{
         authenticated,
